00e7125d793fb948f0c026e2c22c5f107d3082d9
RenHono <renhonoz5@gmail.com>
2016-09-03 09:38:48 +0800

    p2

--- homework902_1/src/com/example/homework902_1/Jieguo.java
+++ homework902_1/src/com/example/homework902_1/Jieguo.java
@@ -7,16 +7,38 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Jieguo extends Activity {
-
+	private long exitTime = 0;
 	private TextView tv2;
 	private TextView tv22;
 	String str = null;
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+			if ((System.currentTimeMillis() - exitTime) > 2000) {
+				Toast.makeText(getApplicationContext(), "再按一次返回主界面", Toast.LENGTH_LONG).show();
+				exitTime = System.currentTimeMillis();
+
+			} else {
+
+				finish();
+				System.exit(0);
+
+			}
+			return true;
+
+		}
+
+		return super.onKeyDown(keyCode, event);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +54,7 @@ public class Jieguo extends Activity {
 		long a = str.hashCode();
 		Random random = new Random(a);
 		int b = (int) Math.abs(random.nextDouble() * 100);
-		tv22.setText(""+str+":您的RP值为："+b);
+		tv22.setText("" + str + ":您的RP值为：" + b);
 	}
 
 	@Override
--- homework902_1/src/com/example/homework902_1/MainActivity.java
+++ homework902_1/src/com/example/homework902_1/MainActivity.java
@@ -4,17 +4,40 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private EditText et;
 	private Button btn;
+	private long exitTime;
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+			if ((System.currentTimeMillis() - exitTime) > 2000) {
+				Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_LONG).show();
+				exitTime = System.currentTimeMillis();
 
+			} else {
+
+				finish();
+				System.exit(0);
+
+			}
+			return true;
+
+		}
+
+		return super.onKeyDown(keyCode, event);
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
