90285f0e1cd2fb4d3ff9550b90d1d554f92407c7
RenHono <renhonoz5@gmail.com>
2016-09-11 18:07:31 +0800

    x

---
+++ homework909_1/src/xyz/renhono/homework909_1/interfaces/ResultCB.java
@@ -0,0 +1,13 @@
+package xyz.renhono.homework909_1.interfaces;
+
+import java.util.List;
+
+import xyz.renhono.homework909_1.domain.Data;
+
+public interface ResultCB {
+
+	
+	
+	void CallBack(List<Data> list);
+	
+}
